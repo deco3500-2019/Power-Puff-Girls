b173b6555d6cbdff674cd1a6a1cd30d2b0218196
Tuvao <s305524@student.hioa.no>
2019-09-23 04:05:31 +0200

    Menu

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Profile from './Profile/Profile';
+import Fridge from './FridgeList/Fridge';
+import GroceryList from './GroceryList/GroceryList';
 
 /* This is the starting page!! */
 
 function App() {
-  return (
+  return (<BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          <TestComponent testProp="Tuva"/>
-          Welcome to the food waste app!
-          <Test2 />
-        </p>
-      </header>
+      <Switch>
+        <Route path="/profile" component={Profile} />
+        <Route path="/fridge" component={Fridge}/>
+        <Route path="/groceryList" component={GroceryList}/>
+      </Switch>
+      <div className="menu">
+        <Link to="/profile" className="menuItem">Profile</Link>
+        <Link to="/fridge" className="menuItem">Fridge</Link>
+        <Link to="/groceryList" className="menuItem">Grocery List</Link>
+      </div>
     </div>
+  </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-class TestComponent extends React.Component{
-  render(){
-    return <h1>Test! {this.props.testProp}</h1>
-  }
-}
-
-const Test2 = () => {
-  return <h1>Test2</h1>
-}
---
+++ src/GroceryList/GroceryList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GroceryList extends React.Component{
+    render(){
+        return <h1>Grocery List</h1>;
+    }
+}
+
+export default GroceryList;
