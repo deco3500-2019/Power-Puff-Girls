884bc189fa427e3b6e7f2df45be2a04574461643
Tuvao <s305524@student.hioa.no>
2019-10-07 10:02:17 +0200

    Expand item with static content

--- src/InventoryList/Inventory.js
+++ src/InventoryList/Inventory.js
@@ -1,40 +1,72 @@
 import React from 'react';
+import './Inventory.css';
 
 class Inventory extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             UserName: 'admin',
             UserPassword: 'admin',
             addItemName: '',
-            searchResults: []
+            searchResults: [],
+            clicked: -1
         }
         this.search = this.search.bind(this);
+        this.expand = this.expand.bind(this);
+        /* setTimeout(
+            fetch('https://s4540545-ppg.uqcloud.net/php_files/inventory_connect.php', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                } 
+            }).then((response) => {
+                console.log(response);
+                return response.json();
+            }).then((responseJson) => {
+                console.log(responseJson);
+            }).catch((error) => {
+                console.log(error);
+            }), 2000); */
     }
-    search(event){
+
+    search(event) {
         const value = event.target.value;
-        const searchResults = dummyListAllItems.filter(({item}) => {
-            return item.toLowerCase().includes(value.toLowerCase()); 
+        const searchResults = dummyListAllItems.filter(({ item }) => {
+            return item.toLowerCase().includes(value.toLowerCase());
         });
         this.setState({
             addItemName: value,
-            searchResults: value === ''? [] : searchResults 
+            searchResults: value === '' ? [] : searchResults
         })
-    }   
+    }
+
+    expand(event) {
+        const id = event.target.id;
+        const { clicked } = this.state;
+        this.setState({
+            clicked: clicked === id ? -1 : id
+        })
+    }
     render() {
+        const { clicked } = this.state;
         return (<div>
             <h1>Inventory</h1>
             <input search="text" placeholder="Add new item..." value={this.state.addItemName}
-                onChange={this.search} name="addItemName"/>
+                onChange={this.search} name="addItemName" />
             <div>{/* Search results section */}
                 {this.state.searchResults.map((result, index) => {
                     return <li key={index}>{result.item}</li>
                 })}
             </div>
-            <hr/>
-            <ul>
-                {dummyListInventoryForUser.map(({id, item}) => {
-                    return <li key={id}>{item}</li>
+            <hr />
+            <ul className="inventory">
+                {dummyListInventoryForUser.map(({ id, item }, index) => {
+                    return <li key={id} className={'inventoryItem ' + (Number(clicked) === id ? 'expandedItem' : '')}
+                        id={id} onClick={this.expand}>
+                        {item}
+                        {Number(clicked) === id? 
+                        <p className="expandedSection">Lorem Ipsum dor si amet</p> : ''}
+                    </li>
                 })}
             </ul>
         </div>
@@ -55,7 +87,7 @@ const dummyListInventoryForUser = [
         item: 'Banana'
     }
 ]
-const dummyListAllItems = [ 
+const dummyListAllItems = [
     {
         id: 1,
         item: 'Avocado',
